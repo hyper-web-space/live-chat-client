3384c8741386c311206345959de2bf88062c82e2
HyunBaeJung <42843734+HyunBaeJung@users.noreply.github.com>
2023-04-12 23:37:08 +0900

    login request func complete / auth lib dev
    ⊙ log-in
    [ v ] layout
    [ v ] login request func
    
    ⊙sign-up
    [ - ] layout
    [ ] sign-up request func
    
    ⊙request lib
    [ v ] set-up
    
    ⊙auth lib
    [ v ] store token
    [ - ] refresh token
    [ ] send token(every request)

--- live-chat-app/src/components/modals/LogIn/LogIn.tsx
+++ live-chat-app/src/components/modals/LogIn/LogIn.tsx
@@ -1,13 +1,13 @@
-import React, { ChangeEvent, SyntheticEvent } from 'react';
+import React, { ChangeEvent } from 'react';
 import './LogIn.scss';
 import logo_white from '../../../../public/logo-white.png';
-import { useSetRecoilState,useRecoilState } from 'recoil';
+import { useSetRecoilState, useRecoilState } from 'recoil';
 import { loginFlag } from '../../../states/flagState';
 import axios from '../../../common/api/axios';
 import requests from '../../../common/api/requests';
+import auth from '../../../common/auth/session';
 
-
-import { userId,userPw } from '../../../states/userState';
+import { userId, userPw } from '../../../states/userState';
 
 
 export default function LogIn() {
@@ -17,13 +17,17 @@ export default function LogIn() {
 
     const [id, setId] = useRecoilState(userId);
     const [pw, setPw] = useRecoilState(userPw);
-    
-    function handleChange(event:ChangeEvent<HTMLInputElement>) {
-        // 새로운 값을 처리하는 로직
-        console.log(event.target.value);
-      }
 
-    function signUpClickHandler(){
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        // 새로운 값을 처리하는 로직
+        if (event.target.id === 'id-input') {
+            setId(event.target.value);
+        } else {
+            setPw(event.target.value);
+        }
+    }
+
+    function signUpClickHandler() {
         setLoginFlag(false);
     }
 
@@ -38,31 +42,33 @@ export default function LogIn() {
     'refreshToken': 'string'
     }
     ************************/
-    async function logInClickHandler(){
+    async function logInClickHandler() {
         //로그인 정보 제출
         // POST 요청은 body에 실어 보냄
-        const res = await axios.post(requests.postLogin, {
-        id      : id,
-        password: pw,
-        });
-        //발급된 JWT는 클라이언트 측에서 저장 - 세션 스토리지
-        
-        //
+        try {
+            const res = await axios.post(requests.postLogin, {
+                id: id,
+                password: pw,
+            });
+            //발급된 JWT는 클라이언트 측에서 저장 - 세션 스토리지
+            auth.setToken('accessToken', res.data.accessToken);
+            auth.setToken('refreshToken', res.data.refreshToken);
+        } catch (error) {
+            alert('later make yeajung');
+        }
     }
 
     return (
-
-
         <div>
             <div className='modal-back-drop'>
             </div>
-            <div className='login-frame' onChange={handleChange}>
-                
+            <div className='login-frame' >
+
                 <div>
                     <img src={logo_white} alt="logo-white" />
                 </div>
-                <input placeholder='ID' type="text" value={id} />
-                <input placeholder='Password' type="password" value={pw} />
+                <input id='id-input' placeholder='ID' type="text" value={id} onChange={handleChange} />
+                <input id='pw-input' placeholder='Password' type="password" value={pw} onChange={handleChange} />
                 <div>
                     <button onClick={logInClickHandler}>로그인</button>
                     <button onClick={signUpClickHandler}>회원가입</button>
