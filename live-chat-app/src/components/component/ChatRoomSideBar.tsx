2ef60533a98221734d5ab9cf5a316cbd67e256a7
HyunBaeJung <42843734+HyunBaeJung@users.noreply.github.com>
2023-04-15 01:06:41 +0900

    메인화면 구성
    main page
    [] ideas
    
    chatroom side bar
    [ v ] layout
    [] copy discord hover effect
    [] D&D action
    [] random image create

--- live-chat-app/src/components/component/ChatRoomSideBar.tsx
+++ live-chat-app/src/components/component/ChatRoomSideBar.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import './ChatRoomSideBar.scss'
+import logo from '../../../public/logo.png';
 
 export default function ChatRoomSideBar() {
+
+  /*
+  @GET /chatRoom
+  [
+  {
+    'chatRoomId': 'string',
+    'name': 'string',
+    'creator': 'string',
+    'numberOfUser': 'number',
+    'privateRoom': 'boolean'
+  },
+  ...
+  ]
+  */
+
+
   return (
     <div className='chat-room-side-bar-wrapper'>
-        tesr
+        <div className='main-button'><img src={logo} alt="logo" /></div>
+        <div className='divier'/>
+        <div className='chat-room-list'>
+            <div className='chat-room'></div>
+            <div className='chat-room'></div>
+            <div className='chat-room'></div>
+            <div className='chat-room'></div>
+        </div>
+
     </div>
   )
 }
